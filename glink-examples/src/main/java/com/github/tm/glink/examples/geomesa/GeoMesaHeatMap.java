30e2f4e773863a5b23caa95b3d7a4c71c4b4a907
haochengw <hihaochengw@outlook.com>
2021-03-28 23:33:33 +0800

    Update heatmap

--- glink-examples/src/main/java/com/github/tm/glink/examples/geomesa/GeoMesaHeatMap.java
+++ glink-examples/src/main/java/com/github/tm/glink/examples/geomesa/GeoMesaHeatMap.java
@@ -1,11 +1,16 @@
 package com.github.tm.glink.examples.geomesa;
 
 import com.github.tm.glink.core.operator.HeatMap;
+import com.github.tm.glink.core.tile.TileGrid;
 import com.github.tm.glink.examples.source.CSVXiamenTrajectorySource;
 import com.github.tm.glink.examples.utils.HBaseCatalogCleaner;
 import com.github.tm.glink.features.TrajectoryPoint;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.datastream.BroadcastStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -14,30 +19,42 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 import java.sql.Timestamp;
 import java.time.Duration;
-import java.util.Objects;
+import java.util.LinkedList;
 
 public class GeoMesaHeatMap {
 
+
     public static void main(String[] args) throws Exception {
-        Time window_length = Time.minutes(30); // 时间窗口长度
-        int h_level = 16;
-        int l_level = 13;  // 默认将设置为0
+        Time window_length = Time.minutes(10); // 时间窗口长度
+        int h_level = 18;
+        int l_level = 12;  // 默认将设置为0
         int speed_up = 120;
         String catalog = "Xiamen_HeatMap";
-        String zookeepers = "localhost:2181"; // 管理HBase的Zookeeper地址
-        String file_path = Objects.requireNonNull(GeoMesaHeatMap.class.getClassLoader()
-                .getResource("XiamenTrajDataCleaned.csv")).getPath(); // 默认从Resources中获取
+        String zookeepers = "u0:2181,u1:2181,u2:2181"; // 管理HBase的Zookeeper地址
+        String file_path = "/mnt/hgfs/disk/dcic/data/origin/XiaMen2019Duanwu.csv";
+//        String zookeepers = "localhost:2181"; // 管理HBase的Zookeeper地址
+//        String file_path = "/Users/haocheng/Code/glink/glink-examples/src/main/resources/XiamenTrajDataCleaned.csv";
 
         // Drop Heatmap tables in HBase
-        HBaseCatalogCleaner.clean("XiamXiamen_HeatMap");
+        HBaseCatalogCleaner.clean("Xiamen_HeatMap");
 
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.getConfig().setAutoWatermarkInterval(1000L);
+
+        LinkedList<TileGrid[]> tileGrids_input = getTileGrids(h_level, l_level);
+
+        // 广播流，广播规则并且创建 broadcast state
+        DataStream<TileGrid[]> tileGridsStream = env.fromCollection(tileGrids_input);
+        MapStateDescriptor<String,TileGrid[]> tileGridsDescriptor = new MapStateDescriptor<String ,TileGrid[]>("TileGridsDescriptor",BasicTypeInfo.STRING_TYPE_INFO,TypeInformation.of(TileGrid[].class));
+        BroadcastStream<TileGrid[]> ruleBroadcastStream = tileGridsStream.broadcast(tileGridsDescriptor);
+
         // 生成流，执行热力图计算
         DataStream<TrajectoryPoint> trajDataStream = env.addSource(new CSVXiamenTrajectorySource(file_path, speed_up))
-                .assignTimestampsAndWatermarks(WatermarkStrategy.<TrajectoryPoint>forBoundedOutOfOrderness(Duration.ofMinutes(2)).withTimestampAssigner((event, timestamp)->event.getTimestamp()));
+                .assignTimestampsAndWatermarks(WatermarkStrategy
+                        .<TrajectoryPoint>forBoundedOutOfOrderness(Duration.ofMinutes(2))
+                        .withTimestampAssigner((event, timestamp)->event.getTimestamp()));
+        DataStream<Tuple4<String, Long, Timestamp, String>> tileStream = HeatMap.GetHeatMap(trajDataStream, h_level,l_level, window_length, ruleBroadcastStream);
 
-        DataStream<Tuple4<String, Long, Timestamp, String>> tileStream = HeatMap.GetHeatMap(trajDataStream, h_level,l_level, window_length);
         // 创建GeoMesa中用于存储HeatMap的表
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
         tableEnv.executeSql(
@@ -62,5 +79,19 @@ public class GeoMesaHeatMap {
         Table tileTable = tableEnv.fromDataStream(tileStream);
         tileTable.executeInsert("Geomesa_HeatMap_Test");
         env.execute("heatmap-generating");
+    }
+    private static LinkedList<TileGrid[]> getTileGrids (int h_level, int l_level) {
+        LinkedList<TileGrid[]> tileGrids_input = new LinkedList<>();
+        int length = h_level-l_level+1;
+        TileGrid[] tileGrids = new TileGrid[length];
+        int i = length;
+        int j = h_level;
+        while (i > 0) {
+            tileGrids[i-1] = new TileGrid(j);
+            i--;
+            j--;
+        }
+        tileGrids_input.add(tileGrids);
+        return tileGrids_input;
     }
 }
